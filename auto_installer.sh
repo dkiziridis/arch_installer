81d6188abcf1ba3a974b8ef4d9afa526fedfc79e
crow <dimikizi@outlook.com>
2018-07-04 12:56:15 +0300

    code fix

--- auto_installer.sh
+++ auto_installer.sh
@@ -1,69 +1,69 @@
 #!/bin/bash
 
-function oh-my-zsh_installer {
+function oh-my-zsh_installer() {
 	echo "Installing Oh-My-Zsh framework"
-	sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" >> /dev/null
+	sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" >>/dev/null
 }
-function driver_installer {
+function driver_installer() {
 	echo "Detecting VGA..."
 	VIDEO=$(lspci | grep -e VGA -e 3D | awk -F ' ' '{print $5}')
-	if [[ $VIDEO = Intel ]]; then
+	if [[ $VIDEO == Intel ]]; then
 		echo "Installing Drivers for $VIDEO..."
-		pacman -S --noconfirm --needed xf86-video-intel	mesa >> /dev/null
-	elif [[ $VIDEO = $VIDEO ]]; then
+		pacman -S --noconfirm --needed xf86-video-intel mesa >>/dev/null
+	elif [[ $VIDEO == $VIDEO ]]; then
 		echo "Installing Drivers for $VIDEO..."
-		pacman -S --noconfirm --needed nvidia	nvidia-utils >> /dev/null
-	elif [[ $VIDEO = ATI ]]; then
+		pacman -S --noconfirm --needed nvidia nvidia-utils >>/dev/null
+	elif [[ $VIDEO == ATI ]]; then
 		echo "Installing Drivers for $VIDEO..."
-		pacman -S --noconfirm --needed xf86-video-ati mesa >> /dev/null
-	elif [[ $VIDEO = Advanced ]]; then
+		pacman -S --noconfirm --needed xf86-video-ati mesa >>/dev/null
+	elif [[ $VIDEO == Advanced ]]; then
 		echo "Installing Drivers for AMD..."
-		pacman -S --noconfirm --needed xf86-video-amdgpu mesa >> /dev/null
-	elif [[ $VIDEO = InnoTek ]]; then
+		pacman -S --noconfirm --needed xf86-video-amdgpu mesa >>/dev/null
+	elif [[ $VIDEO == InnoTek ]]; then
 		echo "Installing Drivers for VirtualBox Graphics..."
-		pacman -S --noconfirm --needed virtualbox-guest-utils virtualbox-guest-modules-arch >> /dev/null
+		pacman -S --noconfirm --needed virtualbox-guest-utils virtualbox-guest-modules-arch >>/dev/null
 	else
 		echo "Graphics Card could not be detected, consult Jim for further instructions..."
 		exit 1
 	fi
-	
+
 	echo "Installing Xorg"
-	pacman -S --noconfirm --needed xorg-server xorg-xauth xf86-input-synaptics xorg-xinit xorg-iceauth xf86-video-fbdev >> /dev/null
+	pacman -S --noconfirm --needed xorg-server xorg-xauth xf86-input-synaptics xorg-xinit xorg-iceauth xf86-video-fbdev >>/dev/null
 	echo "Installing Desktop Environment"
-	pacman -S --needed --noconfirm xfce4 xfce4-appfinder xfce4-power-manager xfce4-session xfce4-settings xfce4-weather-plugin xfce4-xkb-plugin pcmanfm >> /dev/null
+	pacman -S --needed --noconfirm xfce4 xfce4-appfinder xfce4-power-manager xfce4-session xfce4-settings xfce4-weather-plugin xfce4-xkb-plugin pcmanfm >>/dev/null
 }
-function conf_files_root {
+function conf_files_root() {
 	echo "Modifying sudoers file"
 	sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /etc/sudoers
 	echo "Creating empty .zhsrc file"
-	echo " " > /home/$username/.zshrc
+	echo " " >/home/$username/.zshrc
 	chown $username:$username /home/$username/.zshrc
 	echo "Enabling Multilib..."
 	sed -i '93s/#\[multilib\]/\[multilib\]/g' /etc/pacman.conf
 	sed -i '94s/#Include = \/etc\/pacman.d\/mirrorlist/Include = \/etc\/pacman.d\/mirrorlist/g' /etc/pacman.conf
 	echo "Updating repositories..."
-	pacman -Syu --noconfirm >> /dev/null
+	pacman -Syu --noconfirm >>/dev/null
 }
-function user_creation {
+function user_creation() {
 	echo "Creating user."
 	read -p "Enter username : " username
 	useradd -m -G wheel -s /bin/zsh $username
 	echo "Enter password for $username : "
 	passwd $username
 }
-function root_software {
+function root_software() {
 	echo "Installing Zsh"
-	pacman -S --noconfirm --needed zsh >> /dev/null
+	pacman -S --noconfirm --needed zsh >>/dev/null
 	echo "Installing essential software..."
-	pacman -S --noconfirm --needed wget curl git sudo perl tar base-devel alsa-utils >> /dev/null
+	pacman -S --noconfirm --needed wget curl git sudo perl tar base-devel alsa-utils >>/dev/null
 	echo "Installing utilities, this will take a while..."
-	pacman -S --noconfirm --needed android-file-transfer arc-icon-theme bleachbit blueman moc mtr nomacs pkgstats >> /dev/null
-	pacman -S --noconfirm --needed breeze-icons cheese cmatrix deadbeef dstat epdfview fdupes gftp gnome-maps >> /dev/null
-	pacman -S --noconfirm --needed speedtest-cli smartmontools subdl terminus-font tigervnc >> /dev/null
-	pacman -S --noconfirm --needed usbutils volumeicon watchdog weechat x11vnc wget openssh gvim dialog networkmanager >> /dev/null
-	pacman -S --noconfirm --needed gparted gvim htop minitube mlocate qbittorrent smbnetfs smplayer >> /dev/null
+	pacman -S --noconfirm --needed android-file-transfer arc-icon-theme bleachbit blueman moc mtr nomacs pkgstats >>/dev/null
+	pacman -S --noconfirm --needed breeze-icons cheese cmatrix deadbeef dstat epdfview fdupes gftp gnome-maps >>/dev/null
+	pacman -S --noconfirm --needed speedtest-cli smartmontools subdl terminus-font tigervnc >>/dev/null
+	pacman -S --noconfirm --needed usbutils volumeicon watchdog weechat x11vnc wget openssh gvim dialog networkmanager >>/dev/null
+	pacman -S --noconfirm --needed gparted gvim htop minitube mlocate qbittorrent smbnetfs smplayer >>/dev/null
 }
-function conf_files_nonroot {
+function conf_files_nonroot() {
 	echo "Generating xinit file"
 	rm -f /home/$username/.xinitrc
 	echo "#!/bin/sh
@@ -81,66 +81,66 @@ fi
 
 #----XFCE4
 exec startxfce4
-" > /home/$username/.xinitrc
-chown $username:$username /home/$username/.xinitrc
+" >/home/$username/.xinitrc
+	chown $username:$username /home/$username/.xinitrc
 }
-function nonroot_software {
+function nonroot_software() {
 	echo "Signing key 1EB2638FF56C0C53"
-	gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53 >> /dev/null
+	gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53 >>/dev/null
 	echo "Signing key F54984BFA16C640F"
-	gpg --recv-keys --keyserver hkp://pgp.mit.edu F54984BFA16C640F >> /dev/null
+	gpg --recv-keys --keyserver hkp://pgp.mit.edu F54984BFA16C640F >>/dev/null
 	echo "Installing cower-git"
-	wget https://aur.archlinux.org/cgit/aur.git/snapshot/cower.tar.gz >> /dev/null
-	tar -zxvf cower.tar.gz >> /dev/null
+	wget https://aur.archlinux.org/cgit/aur.git/snapshot/cower.tar.gz >>/dev/null
+	tar -zxvf cower.tar.gz >>/dev/null
 	cd cower
-	makepkg --needed --noconfirm -si >> /dev/null
+	makepkg --needed --noconfirm -si >>/dev/null
 	cd ..
 	echo "Installing pacaur"
-	wget https://aur.archlinux.org/cgit/aur.git/snapshot/pacaur.tar.gz >> /dev/null
-	tar -zxvf pacaur.tar.gz >> /dev/null
+	wget https://aur.archlinux.org/cgit/aur.git/snapshot/pacaur.tar.gz >>/dev/null
+	tar -zxvf pacaur.tar.gz >>/dev/null
 	cd pacaur
-	makepkg --needed --noconfirm -si >> /dev/null
+	makepkg --needed --noconfirm -si >>/dev/null
 	cd ..
 	echo "Installing additional software, this will take a while..."
-	pacaur -S --noconfirm --needed --noedit xfce-theme-blackbird xfce-theme-greybird agetpkg-git coolreader3-git dupeguru-me dupeguru-pe dupeguru-se mmv palemoon-bin skype sublime-text teamviewer translate-shell winusb-git discus >> /dev/null
+	pacaur -S --noconfirm --needed --noedit xfce-theme-blackbird xfce-theme-greybird agetpkg-git coolreader3-git dupeguru-me dupeguru-pe dupeguru-se mmv palemoon-bin skype sublime-text teamviewer translate-shell winusb-git discus >>/dev/null
 }
-function services {
+function services() {
 	echo "Enabling services"
-	sudo systemctl enable NetworkManager.service >> /dev/null
+	sudo systemctl enable NetworkManager.service >>/dev/null
 }
 #function clean_up {read -p "Clean up files ? [yn] " yn if [[ $yn = y ]]; then	#TODO fi}
 if [[ "$EUID" -eq 0 ]]; then
-	if ping -q -c 1 -W 1 www.google.com > /dev/null; then
+	if ping -q -c 1 -W 1 www.google.com >/dev/null; then
 		cd ~
-  		user_creation
-  		driver_installer
-  		root_software
-  		conf_files_root
-  		cd /home/$username
-  		SCRIPT="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
-  		cp /root/$SCRIPT /home/$username
-  		chmod u+x /home/$username/$SCRIPT
-  		chown $username:$username /home/$username/$SCRIPT
-  		echo "Reboot, log in to your new account and rerun the script. It was be coppied to your home folder. Just run \"./$SCRIPT\""
-  		read -p "Reboot now ? [yn] : " answer
-  		if [[ $answer = y ]];then
-  			reboot
-  		else
-  			exit 0
-  		fi
+		user_creation
+		driver_installer
+		root_software
+		conf_files_root
+		cd /home/$username
+		SCRIPT="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+		cp /root/$SCRIPT /home/$username
+		chmod u+x /home/$username/$SCRIPT
+		chown $username:$username /home/$username/$SCRIPT
+		echo "Reboot, log in to your new account and rerun the script. It was be coppied to your home folder. Just run \"./$SCRIPT\""
+		read -p "Reboot now ? [yn] : " answer
+		if [[ $answer == y ]]; then
+			reboot
+		else
+			exit 0
+		fi
 	else
-  		echo "Connect to WiFi using \"wifi-menu\" and rerun the script. Fuk u bic."
+		echo "Connect to WiFi using \"wifi-menu\" and rerun the script. Fuk u bic."
 		exit 1
 	fi
 else
-	if ping -q -c 1 -W 1 www.google.com > /dev/null; then
+	if ping -q -c 1 -W 1 www.google.com >/dev/null; then
 		cd ~
-  		nonroot_software
-  		conf_files_nonroot
-  		services
-  		oh-my-zsh_installer
+		nonroot_software
+		conf_files_nonroot
+		services
+		oh-my-zsh_installer
 	else
-  		echo "Connect to WiFi using \"sudo wifi-menu\" and rerun the script. Fuk u bic."
+		echo "Connect to WiFi using \"sudo wifi-menu\" and rerun the script. Fuk u bic."
 		exit 1
 	fi
 fi
--- root_install.sh
+++ root_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-function configure
-{
+configure() {
 	echo "Setting localtime"
 	ln -sf /usr/share/zoneinfo/Europe/Athens /etc/localtime
 	echo "Generating /etc/adjtime"
@@ -11,57 +10,53 @@ function configure
 	echo "Enabling en_US locale"
 	sed -i '/en_US/s/^#//g' /etc/locale.gen
 	echo "Generating locales"
-	locale-gen >> /dev/null
+	locale-gen >>/dev/null
 	echo "Set root password and press Enter"
 	passwd
 }
 
-function driver_installer 
-{
+driver_installer() {
 	echo "Detecting VGA..."
 	VIDEO=$(lspci | grep -e VGA -e 3D | awk -F ' ' '{print $5}')
-	if [[ $VIDEO = Intel ]]; then
+	if [[ $VIDEO == Intel ]]; then
 		echo "Installing Drivers for Intel VGA..."
-		pacman -S --noconfirm --needed xf86-video-intel	mesa >> /dev/null
-	elif [[ $VIDEO = NVIDIA ]]; then
+		pacman -S --noconfirm --needed xf86-video-intel mesa >>/dev/null
+	elif [[ $VIDEO == NVIDIA ]]; then
 		echo "Installing Drivers for $=NVIDIA VGA..."
-		pacman -S --noconfirm --needed nvidia nvidia-utils >> /dev/null
-	elif [[ $VIDEO = ATI ]]; then
+		pacman -S --noconfirm --needed nvidia nvidia-utils >>/dev/null
+	elif [[ $VIDEO == ATI ]]; then
 		echo "Installing Drivers for AMD Legacy VGA..."
-		pacman -S --noconfirm --needed xf86-video-ati mesa >> /dev/null
-	elif [[ $VIDEO = Advanced ]]; then
+		pacman -S --noconfirm --needed xf86-video-ati mesa >>/dev/null
+	elif [[ $VIDEO == Advanced ]]; then
 		echo "Installing Drivers for AMD VGA..."
-		pacman -S --noconfirm --needed xf86-video-amdgpu mesa >> /dev/null
-	elif [[ $VIDEO = InnoTek ]]; then
+		pacman -S --noconfirm --needed xf86-video-amdgpu mesa >>/dev/null
+	elif [[ $VIDEO == InnoTek ]]; then
 		echo "Installing Drivers for VirtualBox Graphics..."
-		pacman -S --noconfirm --needed virtualbox-guest-utils virtualbox-guest-modules-arch >> /dev/null
+		pacman -S --noconfirm --needed virtualbox-guest-utils virtualbox-guest-modules-arch >>/dev/null
 	else
-		echo "Graphics Card could not be detected. Script cannot run."
-		exit 1
+		echo "Graphics Card could not be detected. Press [Enter] to continue."
+		read -r
 	fi
 }
 
-function xorg_installer
-{
-	while read -rn1 -p $'\nInstall X server ? [yn]' ASK
-    do
-    	case "$ASK" in
-        	[Yy] )
-            	echo "Installing Xorg"
-				pacman -S --noconfirm --needed xorg-server xorg-xauth xf86-input-synaptics xorg-xinit xorg-iceauth xf86-video-fbdev >> /dev/null
-                break
-                ;;
-		    [Nn] )
-        		break
-                ;;
-            * )
-        		;;
-    	esac
+xorg_installer() {
+	while read -rn1 -p $'\nInstall X server ? [yn]' ASK; do
+		case "$ASK" in
+		[Yy])
+			echo "Installing Xorg"
+			pacman -S --noconfirm --needed xorg-server xorg-xauth xf86-input-synaptics xorg-xinit xorg-iceauth xf86-video-fbdev >>/dev/null
+			break
+			;;
+		[Nn])
+			break
+			;;
+		*) ;;
+
+		esac
 	done
 }
 
-function de_installer
-{
+de_installer() {
 	echo -ne "\n 1) GNOME \"Modern DE with lots of features.\"
 			  \n 2) KDE Plasma \"Familiar working environment.\"
 			  \n 3) Xfce \"Relatively lightweight and modular.\"
@@ -75,109 +70,104 @@ function de_installer
 			  \nQq) Skip DE install.
 			  "
 
-	while read -rn2 -p $'\nSelect : ' CHAR
-	do
-	    case "$CHAR" in
-	        1 )
-				echo "Installing GNOME 3"
-	            pacman -S --needed --noconfirm gnome >> /dev/null
-	            break
-	            ;;
-	        2 )
-				echo "Installing KDE Plasma"
-	            pacman -S --needed --noconfirm plasma >> /dev/null
-	            break
-	            ;;
-	        3 )
-	            echo "Installing Xfce"
-				pacman -S --needed --noconfirm xfce4 >> /dev/null
-	            break
-	            ;;
-	        4 )
-				echo "Installing Enlightenment"
-	            pacman -S --needed --noconfirm enlightenment >> /dev/null
-	            break
-	            ;;
-	        5 )
-	            echo "Installing LXDE"
-	            pacman -S --needed --noconfirm lxde-gtk3 >> /dev/null
-	            break
-	            ;;
-	        6 )
-	            echo "Installing Cinnamon"
-	            pacman -S --needed --noconfirm cinnamon >> /dev/null
-	            break
-	            ;;
-	        7 )
-	            echo "Installing Deepin"
-	            pacman -S --needed --noconfirm deepin >> /dev/null=-
-	            break
-	            ;;
-	        8 )
-	            echo "Installing GNOME Flashback"
-	            pacman -S --needed --noconfirm gnome-flashback >> /dev/null
-	            break
-	            ;;
-	        9 )
-	            echo "Installing MATE"
-	            pacman -S --needed --noconfirm mate >> /dev/null
-	            break
-	            ;;
-	        10 )
-	            echo "Installing Budgie"
-	            pacman -S --needed --noconfirm budgie-desktop >> /dev/null
-	            break
-	            ;;
-	        [Qq] )
-				break
-	            ;;
-	        * 	 )
-	            ;;
-	    esac
+	while read -rn2 -p $'\nSelect : ' CHAR; do
+		case "$CHAR" in
+		1)
+			echo "Installing GNOME 3"
+			pacman -S --needed --noconfirm gnome >>/dev/null
+			break
+			;;
+		2)
+			echo "Installing KDE Plasma"
+			pacman -S --needed --noconfirm plasma >>/dev/null
+			break
+			;;
+		3)
+			echo "Installing Xfce"
+			pacman -S --needed --noconfirm xfce4 >>/dev/null
+			break
+			;;
+		4)
+			echo "Installing Enlightenment"
+			pacman -S --needed --noconfirm enlightenment >>/dev/null
+			break
+			;;
+		5)
+			echo "Installing LXDE"
+			pacman -S --needed --noconfirm lxde-gtk3 >>/dev/null
+			break
+			;;
+		6)
+			echo "Installing Cinnamon"
+			pacman -S --needed --noconfirm cinnamon >>/dev/null
+			break
+			;;
+		7)
+			echo "Installing Deepin"
+			pacman -S --needed --noconfirm deepin >>/dev/null=-
+			break
+			;;
+		8)
+			echo "Installing GNOME Flashback"
+			pacman -S --needed --noconfirm gnome-flashback >>/dev/null
+			break
+			;;
+		9)
+			echo "Installing MATE"
+			pacman -S --needed --noconfirm mate >>/dev/null
+			break
+			;;
+		10)
+			echo "Installing Budgie"
+			pacman -S --needed --noconfirm budgie-desktop >>/dev/null
+			break
+			;;
+		[Qq])
+			break
+			;;
+		*) ;;
+
+		esac
 	done
 }
 
-function util_installer
-{
-	echo "Installing sudo"
+util_installer() {
+	echo "Installing utilities"
 	pacman -S --noconfirm --needed sudo git parted grub
 }
 
-function grub_install
-{
-    parted /dev/sda print | grep EFI >> /dev/null
-    if [[ 0 -eq $? ]]; then
-        echo "Installing GRUB for UEFI systems"
-        grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB >> /dev/null
-        echo "Generating GRUB configuration"
-        grub-mkconfig -o /boot/grub/grub.cfg >> /dev/null
-    else
-        echo "Installing GRUB for BIOS systems"
-        grub-install --target=i386-pc /dev/$_DISK >> /dev/null
-        echo "Generating GRUB configuration"
-        grub-mkconfig -o /boot/grub/grub.cfg >> /dev/null
-    fi
+grub_install() {
+	parted /dev/sda print | grep EFI >>/dev/null
+	if [[ 0 -eq $? ]]; then
+		echo "Installing GRUB for UEFI systems"
+		grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB >>/dev/null
+		echo "Generating GRUB configuration"
+		grub-mkconfig -o /boot/grub/grub.cfg >>/dev/null
+	else
+		echo "Installing GRUB for BIOS systems"
+		grub-install --target=i386-pc /dev/$_DISK >>/dev/null
+		echo "Generating GRUB configuration"
+		grub-mkconfig -o /boot/grub/grub.cfg >>/dev/null
+	fi
 }
 
-function user_creation 
-{
+user_creation() {
 	echo "Creating user"
 	read -p "Enter username : " username
-	useradd -m -G wheel -s /bin/bash $username
+	useradd -m -G wheel -s /bin/bash "$username"
 	echo "Enter password for $username : "
-	passwd $username
+	passwd "$username"
 }
 
-function network_conf
-{
+network_conf() {
 	read -rp $'\nEnter a hostname and press enter. : ' hostname
-	echo $hostname >> /etc/hostname >> /dev/null
+	echo "$hostname" >>/etc/hostname >>/dev/null
 	pacman -S --noconfirm --needed networkmanager dialog
-	sudo systemctl enable NetworkManager.service >> /dev/null
+	sudo systemctl enable NetworkManager.service >>/dev/null
 }
 
 clear
-cd
+cd ~
 
 configure
 user_creation
